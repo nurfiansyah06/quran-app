1199d2ea8e7789e7b233aca2e98554fd9d37f526
Muhamad Nur Arifiansyah <nurfiansyah06@gmail.com>
2025-02-07 17:04:03 +0700

    feat: add search filter for surah

--- src/App.js
+++ src/App.js
@@ -3,39 +3,64 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Table } from "react-bootstrap";
 
 const App = () => {
+  const searchFile = {
+    backgroundPosition: "10px 12px",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    fontSize: "16px",
+    padding: "12px 20px 12px 40px",
+    border: "1px solid #ddd",
+    marginBottom: "12px",
+  };
+
   const [surahs, setSurahs] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("https://api.alquran.cloud/v1/surah")
       .then((response) => response.json())
-      .then((json) => setSurahs(json.data));
+      .then((json) => setSurahs(json.data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  console.log(surahs.data);
+  const filteredSurahs = surahs.filter(
+    (surah) =>
+      surah.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      surah.englishName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <main className="my-5 py-5">
       <Container className="px-0">
         <h1 className="display-4">Quran App</h1>
-        <Table striped bordered hover>
+        
+        <input
+          type="text"
+          style={searchFile}
+          placeholder="Search Surah"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        
+        <Table striped bordered hover id="myTable">
           <thead>
             <tr>
-              <th>Name</th>
+              <th>Arabic Name</th>
               <th>English Name</th>
             </tr>
           </thead>
           <tbody>
-            {surahs.length > 0 ? (
-              surahs.map((name) => (
-                <tr key={name.id}>
-                  <td>{name.name}</td>
-                  <td>{name.englishName}</td>
+            {filteredSurahs.length > 0 ? (
+              filteredSurahs.map((surah) => (
+                <tr key={surah.number}>
+                  <td>{surah.name}</td>
+                  <td>{surah.englishName}</td>
                 </tr>
               ))
             ) : (
               <tr>
-                <td colSpan="4" className="text-center">
-                  No files available
+                <td colSpan="2" className="text-center">
+                  No surahs available
                 </td>
               </tr>
             )}
